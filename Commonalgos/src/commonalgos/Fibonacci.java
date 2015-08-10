fa74d455c38b9b7aa1476ea1f0127064522b5678
yeshreddy <yeswanth.bhoomireddy@gmail.com>
2015-08-10 19:07:56 +0530

    Fibonacci added

--- Commonalgos/src/commonalgos/Fibonacci.java
+++ Commonalgos/src/commonalgos/Fibonacci.java
@@ -22,5 +22,19 @@ public class Fibonacci {
         }
         return sum;
     }
+    public int _find_nth_fibonacci_recursive(int n){
+        if(n<=1) return n;
+        return  _find_nth_fibonacci_recursive(n-1)+ _find_nth_fibonacci_recursive(n-2);
+    }
+    public int _find_nth_fibonacci_dp(int n){
+        if(n<=1) return n;
+        int arr[]=new int[n+1];
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2;i<=n;++i){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[n];
+    }
     
 }
